07ba9356958af658f5e8669888c7311b57016649
Sergio Alba <sergioalba.1099@gmail.com>
2023-11-28 11:52:18 -0400

    añadiendo boton de modos de tema

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/header/header';
 import Router from './navigation';
 import Footer from './components/footer/footer';
 import images from './assets/images';
+import ModeTheme from './components/ModeTheme/modeTheme';
 
 function App() {
   return (
     <div>
       <Header logo={images.logo}/>
+      <ModeTheme/>
       <Router/>
       <Footer/>
     </div>
--- src/assets/images.js
+++ src/assets/images.js
@@ -1,11 +1,13 @@
-import logo from './Logo(1).png';
+import logo from './Logo.png';
+import bgcircle from './img/background_circle.png'
 import img1 from './img/e867c75fc4bd0682ccf080b88dae859f.png';
 import github from './Logos/git-hub.png';
 import linkedin from './Logos/LinkedIn_logo_initials.png'
 
 export default {
   'logo':logo,
-  'img1':img1,
   'github':github,
   'linkedin':linkedin,
+  'bgcircle':bgcircle,
+  'img1':img1,
 }
